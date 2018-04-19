2b687af0cca8e7af677696ab564205a525aa55ca
Zlika <zlika_ese@hotmail.com>
2018-04-19 23:00:54 +0200

    Add files for android build

---
+++ libretro/jni/Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+CORE_DIR = $(LOCAL_PATH)/../..
+
+include $(CORE_DIR)/libretro/Makefile.common
+
+COREFLAGS := -std=gnu99 -DANDROID -D__LIBRETRO__ $(INCFLAGS)
+
+GIT_VERSION := " $(shell git rev-parse --short HEAD || echo unknown)"
+ifneq ($(GIT_VERSION)," unknown")
+	COREFLAGS += -DGIT_VERSION=\"$(GIT_VERSION)\"
+endif
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := retro
+LOCAL_SRC_FILES    := $(SOURCES_C)
+LOCAL_CFLAGS       := $(COREFLAGS)
+LOCAL_LDFLAGS      := -Wl,-version-script=$(CORE_DIR)/libretro/link.T
+LOCAL_LDLIBS       := -lz
+include $(BUILD_SHARED_LIBRARY)
+
