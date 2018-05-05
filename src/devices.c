b104f24f37833422dcc873c85c8e84ae5a730455
Zlika <zlika_ese@hotmail.com>
2018-05-05 15:38:40 +0200

    Fix coverity warnings

--- src/devices.c
+++ src/devices.c
@@ -84,10 +84,10 @@ static void Readsector(void)
   p = Mgetc(0x604b) & 0xff; if(p > 79) {Diskerror(53); return;}
   s = Mgetc(0x604c) & 0xff; if((s == 0) || (s > 16)) {Diskerror(53); return;}
   s += 16 * p + 1280 * u;
-  fseek(ffd, 0, SEEK_END);
+  if (fseek(ffd, 0, SEEK_END)) {Diskerror(53); return;}
   if((s << 8) > ftell(ffd)) {Diskerror(53); return;}
   for(j = 0; j < 256; j++) buffer[j] = 0xe5;
-  fseek(ffd, (s - 1) << 8, SEEK_SET);
+  if (fseek(ffd, (s - 1) << 8, SEEK_SET)) {Diskerror(53); return;}
   i = ((Mgetc(0x604f) & 0xff) << 8) + (Mgetc(0x6050) & 0xff);
   if(fread(buffer, 256, 1, ffd) == 0) {Diskerror(53); return;}
   for(j = 0; j < 256; j++) Mputc(i++, buffer[j]);
@@ -106,7 +106,7 @@ static void Writesector(void)
   p = Mgetc(0x604b) & 0xff; if(p > 79) {Diskerror(53); return;}
   s = Mgetc(0x604c) & 0xff; if((s == 0) || (s > 16)) {Diskerror(53); return;}
   s += 16 * p + 1280 * u;
-  fseek(ffd, (s - 1) << 8, SEEK_SET);
+  if (fseek(ffd, (s - 1) << 8, SEEK_SET)) {Diskerror(53); return;}
   i = 256 * (Mgetc(0x604f) & 0xff) + (Mgetc(0x6050) & 0xff);
   for(j = 0; j < 256; j++) buffer[j] = Mgetc(i++);
   if(fwrite(buffer, 256, 1, ffd) == 0) Diskerror(53);
@@ -126,19 +126,19 @@ static void Formatdisk(void)
   //rem: fatlength provisoire !!!!! (tester la variable adequate)
   // Initialisation of the whole disk with 0xE5
   for(i = 0; i < 256; i++) buffer[i] = 0xe5;
-  fseek(ffd, u, SEEK_SET);
+  if (fseek(ffd, u, SEEK_SET)) {Diskerror(53); return;}
   for(i = 0; i < (fatlength * 8); i++)
     if(fwrite(buffer, 256, 1, ffd) == 0) {Diskerror(53); return;}
   // Initialisation of track 20 at 0xFF
   for(i = 0; i < 256; i++) buffer[i] = 0xff;
-  fseek(ffd, u + 0x14000, SEEK_SET);
+  if (fseek(ffd, u + 0x14000, SEEK_SET)) {Diskerror(53); return;}
   for(i = 0; i < 16; i++)
     if(fwrite(buffer, 256, 1, ffd) == 0) {Diskerror(53); return;}
   // Write the FAT
   buffer[0x00] = 0;
   buffer[0x29] = 0xfe; buffer[0x2a] = 0xfe;
   for(i = fatlength + 1; i < 256; i++) buffer[i] = 0xfe;
-  fseek(ffd, u + 0x14100, SEEK_SET);
+  if (fseek(ffd, u + 0x14100, SEEK_SET)) {Diskerror(53); return;}
   if(fwrite(buffer, 256, 1, ffd) == 0) {Diskerror(53); return;}
 }
 
