29dfa8a7aaf37555201b2a93da17af7a296e6ca0
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2020-05-22 11:58:33 +0200

    Adapt VKeyboard for supporting AGBR1555 and RGB565. Phyton script has been changed and the generated bin files

--- src/vkeyb/tools/img2c.py
+++ src/vkeyb/tools/img2c.py
@@ -7,6 +7,9 @@ from PIL import Image
 def rgba888ToRgb565(rgba):
     return ((rgba[0] >> 3) << 11) | ((rgba[1] >> 2) << 5) | (rgba[2] >> 3)
 
+def rgba888ToAbgr1555(rgba):
+    return ((rgba[2] >> 3) << 10) | ((rgba[1] >> 3) << 5) | (rgba[0] >> 3)
+
 filename = sys.argv[1]
 img = Image.open(filename).convert('RGB')
 pixels = img.load()
@@ -17,6 +20,21 @@ print("const int " + c_var_base_name + "_IMG_WIDTH = " + str(img.width) + ";")
 print("const int " + c_var_base_name + "_IMG_HEIGHT = " + str(img.height) + ";")
 print("")
 
+print("#if defined(SUPPORT_ARGB1555)")
+print("const uint16_t " + c_var_base_name + "_IMG_DATA[] =")
+print("{")
+for y in range(0, img.height):
+    print("  ", end='')
+    for x in range(0, img.width):
+        print('0x{0:0{1}X}'.format(rgba888ToAbgr1555(pixels[x, y]), 4), end='')
+        if ((y != img.height - 1) or (x != img.width - 1)):
+           print(",", end='')
+    print("")
+print("};")
+
+print("")
+
+print("#else")
 print("const uint16_t " + c_var_base_name + "_IMG_DATA[] =")
 print("{")
 for y in range(0, img.height):
@@ -27,3 +45,4 @@ for y in range(0, img.height):
            print(",", end='')
     print("")
 print("};")
+print("#endif")
